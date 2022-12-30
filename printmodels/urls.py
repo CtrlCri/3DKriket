a562fabe980ba50ef7d4e23c6bca81cf9d4add80
armycrih <armycrih@gmail.com>
2022-12-30 13:35:45 -0300

    printmodels makemigrations

--- printmodels/urls.py
+++ printmodels/urls.py
@@ -1,19 +1,21 @@
-from rest_framework.routers import DefaultRouter
-from .views import ( 
-    PrintModelViewSet, SampleImageViewSet, 
-    DownloadableFileViewSet, TagViewSet, 
-    PrintModelTagViewSet, PrintModelViewSet, 
-    CategoryViewSet, OrderViewSet, 
-    ReviewViewSet, PaymentViewSet
-)
-router = DefaultRouter()
+# Archivo principal de urls
 
-router.register(r'print-models', PrintModelViewSet)
-router.register(r'sample-images', SampleImageViewSet)
-router.register(r'downloadable-files', DownloadableFileViewSet)
-router.register(r'tags', TagViewSet)
-router.register(r'print-model-tags', PrintModelTagViewSet)
-router.register(r'categories', CategoryViewSet)
-router.register(r'orders', OrderViewSet)
-router.register(r'reviews', ReviewViewSet)
-router.register(r'payments', PaymentViewSet)
+from django.urls import include, path
+from rest_framework import routers
+
+from printmodels import views
+
+router = routers.DefaultRouter()
+router.register(r'print-models', views.PrintModelViewSet, basename='print-model')
+router.register(r'sample-images', views.SampleImageViewSet, basename='sample-image')
+router.register(r'downloadable-files', views.DownloadableFileViewSet, basename='downloadable-file')
+router.register(r'tags', views.TagViewSet, basename='tag')
+router.register(r'print-model-tags', views.PrintModelTagViewSet, basename='print-model-tag')
+router.register(r'categories', views.CategoryViewSet, basename='category')
+router.register(r'orders', views.OrderViewSet, basename='order')
+router.register(r'reviews', views.ReviewViewSet, basename='review')
+router.register(r'payments', views.PaymentViewSet, basename='payment')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
