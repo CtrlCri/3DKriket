0e32f6a4327e49bfb61c4fd953f935fd61326554
armycrih <armycrih@gmail.com>
2023-01-03 02:49:35 -0300

    users models update

--- users/models.py
+++ users/models.py
@@ -6,32 +6,27 @@ from django.contrib.auth.models import (
 )
 
 class UserManager(BaseUserManager):
-    
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('Email fails')
-        user = self.model(email=email, **extra_fields)
+            raise ValueError('El email es obligatorio')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-        
         return user
-    
+
     def create_superuser(self, email, password):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-        
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
-    name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    
+
     objects = UserManager()
-    
+
     USERNAME_FIELD = 'email'
-    
-    def get_name(self):
-        return self.name
+    REQUIRED_FIELDS = []
