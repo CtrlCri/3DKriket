7a2169b355b771155ee990009e9a7de72ac28f10
armycrih <armycrih@gmail.com>
2023-01-25 12:22:52 -0300

    create models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from base.models import BaseModel
 
-# Create your models here.
+class Category(BaseModel):
+    """
+    Modelo de categoria
+    """
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Product(BaseModel):
+    """
+    Modelo de producto
+    """
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(BaseModel):
+    """
+    Modelo de cliente
+    """
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Purchase(BaseModel):
+    """
+    Modelo de compra
+    """
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Purchase by {self.customer.name} for product {self.product.name}"
+
+class Complaint(BaseModel):
+    """
+    Modelo de queja
+    """
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    complaint_category = models.CharField(max_length=255)
+    description = models.TextField()
+    resolution = models.TextField()
+
+    def __str__(self):
+        return f"Complaint by {self.purchase.customer.name} for purchase {self.purchase.id}"
+
